88380d9e2aef8a9acec7e8a8bacf082667dffc3f
shriyans kapoor <shriyanskapoor@shriyanss-MBP.Dlink>
2021-12-04 21:19:30 +0530

    rwtbre

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3000;
 var app = express();
 
 // app.use(cors())
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://nse-api-55685.herokuapp.com/'], credentials: true }));
+
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
