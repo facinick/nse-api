18db5d7803880c9156d4a48dcd61ca3905c5ae74
shriyans kapoor <shriyanskapoor@shriyanss-MBP.Dlink>
2021-12-05 02:56:28 +0530

    cors headers check

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var app = express();
 app.use(cors());
 
 app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
     //respond with 200
